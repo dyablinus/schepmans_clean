2f37aa7ef21c9bb5edf16c74f7c45bd87e3e1c3d
jerem jerem <jerem jerem>
2017-05-12 09:50:17 +0200

    corrective icone footer

--- application/views/template/base/footer.php
+++ application/views/template/base/footer.php
@@ -1,53 +1,59 @@
-<footer id="footerarnault">
-    <ul class="nav navbar-nav navbar-left">
-        <li class="facebook"><a href="#" title="Facebook"><i class="fa fa-facebook-official" aria-hidden="true"></i>
+<div class="container">
+
+    <footer id="footerarnault">
+        <ul class="nav navbar-nav navbar-left nav_footer">
+            <li class="facebook"><a href="#" title="Facebook"><i class="fa fa-facebook-official" aria-hidden="true"></i>
                 <span></span></a></li>
-        <li class="twitter"><a href="#" title="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i>
+            <li class="twitter"><a href="#" title="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i>
                 <span></span></a></li>
-        <li class="youtube"><a href="#" title="Youtube"><i class="fa fa-youtube" aria-hidden="true"></i>
+            <li class="youtube"><a href="#" title="Youtube"><i class="fa fa-youtube" aria-hidden="true"></i>
                 <span></span></a></li>
-        <li class="actu"><a href="#" title="Actu"><i class="fa fa-rss" aria-hidden="true"></i>
+            <li class="actu_footer"><a href="#" title="Actu"><i class="fa fa-rss" aria-hidden="true"></i>
                 <span></span></a></li>
-    </ul>
-    <h5 id="copyright">
-        <p>Copyright Molengeek 2017</p>
-    </h5>
-    <h3 id="subscrite">
-        <p>Subscribe to the Newsletter: </p>
-    </h3>
+        </ul>
 
 
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-lg btncontact" id="buttonfoot" data-toggle="modal" data-target="#myModal">Je m'inscris</button>
-    <!-- Modal -->
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Suivez toute l'actualité de Françoise Schepmans</h4>
-                </div>
-                <div class="modal-body">
-                    <div class="container">
-                        <div class="col-md-6">
-                            <input type="text" class="form-control" id="" placeholder="Votre email*">
-                            <input type="text" class="form-control" id="" placeholder="Prénom">
-                            <input type="text" class="form-control" id="" placeholder="nom">
-                        </div>
-                        <div class="col-md-12">
-                            <h6><small>*  Champ requis</small></h6>
+        <div id="text_footer">
+            <div id="copyright">
+                <p>Copyright Molengeek 2017</p>
+            </div>
+            <div id="subscrite">
+                <p>Subscribe to the Newsletter: </p>
+            </div>
+        </div>
+
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-lg btncontact" id="buttonfoot" data-toggle="modal" data-target="#myModal">Je m'inscris</button>
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Suivez toute l'actualité de Françoise Schepmans</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="container">
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" id="" placeholder="Votre email*">
+                                <input type="text" class="form-control" id="" placeholder="Prénom">
+                                <input type="text" class="form-control" id="" placeholder="nom">
+                            </div>
+                            <div class="col-md-12">
+                                <h6><small>*  Champ requis</small></h6>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary btncar">S'inscrire</button>
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary btncar">S'inscrire</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 
 
-    <br>
-</footer>
+        <br>
+    </footer>
+</div>
--- application/views/template/base/nav.php
+++ application/views/template/base/nav.php
@@ -1,32 +1,31 @@
-
 <nav class="navbar navbar-default" id="background-blank" role="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
-       <div class="row">
-       <div class="col-md-6 schep">
+    <div class="row">
+        <div class="col-md-6 schep">
 
-        <ul class="nav navbar-nav">
-<li>
+            <ul class="nav navbar-nav">
+                <li>
 
-       <img src="<?= base_url("assets/images/schepmans_logo.jpg")?>" id="scheplogo" alt="" title="Logo">
-</li>
-        </ul>
-       </div>
-    <div class="col-md-6 sociaux">
-        <!-- réseaux sociaux -->
-        <ul class="nav navbar-nav navbar-right">
-<li class="facebook"><a href="#" title="Facebook"><i class="fa fa-facebook-official" aria-hidden="true"></i>
+                    <img src="<?= base_url("/assets/images/schepmans_logo.jpg ")?>" id="scheplogo" alt="" title="Logo">
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-6 sociaux">
+            <!-- réseaux sociaux -->
+            <ul class="nav navbar-nav navbar-right">
+                <li class="facebook"><a href="#" title="Facebook"><i class="fa fa-facebook-official" aria-hidden="true"></i>
 <span></span></a></li>
-	<li class="twitter"><a href="#" title="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i>
+                <li class="twitter"><a href="#" title="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i>
 <span></span></a></li>
-<li class="youtube"><a href="#" title="Youtube"><i class="fa fa-youtube" aria-hidden="true"></i>
+                <li class="youtube"><a href="#" title="Youtube"><i class="fa fa-youtube" aria-hidden="true"></i>
 <span></span></a></li>
-<li class="actu"><a href="#" title="Actu"><i class="fa fa-rss" aria-hidden="true"></i>
+                <li class="actu"><a href="#" title="Actu"><i class="fa fa-rss" aria-hidden="true"></i>
 <span></span></a></li>
-        </ul>
+            </ul>
+        </div>
     </div>
-       </div>
 
-   
+
 </nav>
 
 <nav class="navbar navbar-default pad" role="navigation">
@@ -42,36 +41,55 @@
     </div>
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse navbar-ex1-collapse">
-       
-                    <div class="row">
-                        <div class="col-sm-1"></div>
-                    <div class="col-sm-2">
 
-                    <p class="hovernav"><a href="<?= base_url ("schepmans") ?>">HOME</a></li>
-                    </div>
-                    <div class="col-sm-2">
-                        
-                    <p class="hovernav"><a href="<?= base_url ("schepmans") ?>">QUI SUIS-JE?</a></li>
-                    </div>
-                    <div class="col-sm-2">
-                        
-                    <p class="hovernav"><a href="<?= base_url ("schepmans") ?>">VIE POLITIQUE</a></li>
-                    </div>
-                    <div class="col-sm-2">
-                        
-                    <p class="hovernav"><a href="<?= base_url ("schepmans") ?>">MOLENBEEK</a></li>
-                    </div>
-                     <div class="col-sm-2">
-                        
-                    <p class="hovernav"><a href="<?= base_url ("schepmans") ?>">CONTACT</a></li>
-                    </div>  
-                    <div class="col-sm-1"></div>
-                    </div>
-    
-    </div><!-- /.navbar-collapse -->
+
+        <div class="row">
+            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+
+            </div>
+            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+
+                <li class="hovernav"><a href="<?= base_url ("/schepmans/index ") ?>">HOME</a></li>
+            </div>
+            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+
+                <li class="hovernav"><a href="<?= base_url ("/schepmans/about ") ?>">QUI SUIS-JE?</a></li>
+            </div>
+            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+
+                <li class="hovernav"><a href="<?= base_url ("/schepmans/actualite ") ?>">Actualités</a></li>
+            </div>
+            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+
+                <li class="hovernav"><a href="<?= base_url ("/schepmans ") ?>">MOLENBEEK</a></li>
+            </div>
+            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+
+                <li class="hovernav"><a href="<?= base_url ("/schepmans ") ?>">CONTACT</a></li>
+            </div>
+            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+
+            </div>
+        </div>
+  
+    <!-- /.navbar-collapse -->
 </nav>
 
-
-
-
-
+<!--
+<nav class="navbar navbar-default navbar-static-top" role="navigation">
+    <a class="navbar-brand" href="#">Title</a>
+    <ul class="nav navbar-nav">
+        <li>
+            <a href="<?= base_url(" /schepmans/index.php ")?>">HOME</a>
+        </li>
+        <li>
+            <a href="<?= base_url(" /schepmans/index.php ")?>">HOME</a>
+        </li>
+        <li>
+            <a href="<?= base_url(" /schepmans/index.php ")?>">HOME</a>
+        </li>
+        <li>
+            <a href="<?= base_url(" /schepmans/index.php ")?>">HOME</a>
+        </li>
+    </ul>
+</nav>-->
