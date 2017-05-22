15cbf2eb4ebf067212d868ebf8c8ecbf9b265d05
Talouf <sylhorczak@gmail.com>
2017-05-22 16:03:19 +0200

    background color + shadow

--- application/views/template/base/index.php
+++ application/views/template/base/index.php
@@ -27,7 +27,7 @@
 
 
 </head>
-<body style="background-image:linear-gradient(rgba(27, 27, 255, 0.73), white);">
+<body style="background-image: linear-gradient(rgba(156, 156, 255, 0.19), white);">
 
 <div style="background-color: white; box-shadow: -1em 0 125px black, 1em 0 125px black;" class="container">
 <?php  $this->load->view('template/base/nav'); ?> 
