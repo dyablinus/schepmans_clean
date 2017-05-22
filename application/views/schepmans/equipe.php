26e883f2c2f129d464e0460bb56ffc04aa156859
jerem jerem <jerem jerem>
2017-05-22 15:31:34 +0200

    fin page equipe ajout d 'un membre, changement titre

--- application/views/schepmans/equipe.php
+++ application/views/schepmans/equipe.php
@@ -5,14 +5,14 @@
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/1.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/1.jpg ") ?>" alt="">
 
         </div>
 
         <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
             <div class="elue_equipe">Patricia Vande Maele</div>
             <div class="role_equipe">Echevine</div>
-            <div class="email_equipe">particia.vandemaele@gmail.com</div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>particia.vandemaele@gmail.com</div>
         </div>
 
     </div>
@@ -21,14 +21,14 @@
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/2.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/2.jpg ") ?>" alt="">
 
         </div>
 
         <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
             <div class="elue_equipe">Jan Gypers</div>
             <div class="role_equipe">Echevine</div>
-            <div class="email_equipe">gypers2012@gmail.com</div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>gypers2012@gmail.com</div>
         </div>
 
     </div>
@@ -37,19 +37,19 @@
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/3.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/3.jpg ") ?>" alt="">
 
         </div>
 
         <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
             <div class="elue_equipe">Olivier Mahy</div>
             <div class="role_equipe">Echevin</div>
-            <div class="email_equipe">oliviermahy2012@gmail.com</div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>oliviermahy2012@gmail.com</div>
         </div>
 
     </div>
 
-<!--END ROW 1-->
+    <!--END ROW 1-->
 </div>
 
 
@@ -59,14 +59,14 @@
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/4.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/4.jpg ") ?>" alt="">
 
         </div>
 
         <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
             <div class="elue_equipe">Dina Bastin</div>
             <div class="role_equipe">Conseillière communale et CPAS</div>
-            <div class="email_equipe">dina.bastin@base.be</div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>dina.bastin@base.be</div>
         </div>
 
     </div>
@@ -75,7 +75,7 @@
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/5.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/5.jpg ") ?>" alt="">
 
         </div>
 
@@ -91,19 +91,19 @@
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/6.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/6.jpg ") ?>" alt="">
 
         </div>
 
         <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
             <div class="elue_equipe">Michel Eylenbosh</div>
             <div class="role_equipe">Président du Conseil communal</div>
-            <div class="email_equipe">michel.eylenbosh@skynet.be</div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>michel.eylenbosh@skynet.be</div>
         </div>
 
     </div>
 
-<!--END ROW 2-->
+    <!--END ROW 2-->
 </div>
 
 
@@ -113,14 +113,14 @@
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/7.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/7.jpg ") ?>" alt="">
 
         </div>
 
         <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
             <div class="elue_equipe">Dany Evraud</div>
             <div class="role_equipe">Conseillère communale</div>
-            <div class="email_equipe">danielle.evraud@skynet.be</div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>danielle.evraud@skynet.be</div>
         </div>
 
     </div>
@@ -129,14 +129,14 @@
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/8.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/8.jpg ") ?>" alt="">
 
         </div>
 
         <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
             <div class="elue_equipe">Shazia Manzoor</div>
             <div class="role_equipe">Conseillère communale et de police</div>
-            <div class="email_equipe">ms1328@hotmail.com</div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>ms1328@hotmail.com</div>
         </div>
 
     </div>
@@ -145,19 +145,19 @@
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/9.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/9.jpg ") ?>" alt="">
 
         </div>
 
         <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
             <div class="elue_equipe">Georges Van Leeckwyck</div>
             <div class="role_equipe">Conseiller communal </div>
-            <div class="email_equipe">g.vanleeckwyck@skynet.be</div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>g.vanleeckwyck@skynet.be</div>
         </div>
 
     </div>
 
-<!--END ROW 3-->
+    <!--END ROW 3-->
 </div>
 
 
@@ -167,106 +167,48 @@
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/10.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/10.jpg ") ?>" alt="">
 
         </div>
 
         <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
             <div class="elue_equipe">Léonidas PAPADIZ</div>
             <div class="role_equipe">Conseiller communal et de police </div>
-            <div class="email_equipe">leopapadiz@hotmail.com</div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>leopapadiz@hotmail.com</div>
         </div>
 
     </div>
 
 
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/11.jpg") ?>" alt="">
-
-        </div>
-
-        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
-            <div class="elue_equipe">Luc Léonard</div>
-            <div class="role_equipe">Conseiller communal - Chef du groupe MR au conseil communal </div>
-            <div class="email_equipe">luccjleonard@gmail.com</div>
-        </div>
-
-    </div>
-
-
-    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/12.jpg") ?>" alt="">
-
-        </div>
-
-        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
-            <div class="elue_equipe">Laurie Carême - Palanga</div>
-            <div class="role_equipe">Conseillère communale </div>
-            <div class="email_equipe">loredana1955@scarlet.be</div>
-        </div>
-
-    </div>
-
-<!--END ROW 4-->
-</div>
-
-
-<!--DEBUT ROW-->
-<div class="row row_equipe text-center">
-
-    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/13.jpg") ?>" alt="">
-
-        </div>
-
-        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
-            <div class="elue_equipe">Mohamed El Abboudi</div>
-            <div class="role_equipe">Conseiller communal et de police </div>
-            <div class="email_equipe">mo-elabboudi@hotmail.com</div>
-        </div>
-
-    </div>
-
-
-    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/14.jpg") ?>" alt="">
-
-        </div>
-
-        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
-            <div class="elue_equipe">Pascal Gardinal</div>
-            <div class="role_equipe">Conseiller CPAS - Secrétaire et trésorier de la section MR de Molenbeek-Saint-Jean </div>
-            <div class="email_equipe">pascal.gardinal@skynet.be</div>
-        </div>
-
-    </div>
-
-
-    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/15.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/15.jpg ") ?>" alt="">
 
         </div>
 
         <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
             <div class="elue_equipe">Naïma Benbelkheir</div>
             <div class="role_equipe">Consellère CPAS </div>
-            <div class="email_equipe">benbelkheir.naima@gmail.com
-</div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>benbelkheir.naima@gmail.com</div>
+        </div>
+    </div>
+
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/12.jpg ") ?>" alt="">
+
+        </div>
+
+        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
+            <div class="elue_equipe">Laurie Carême - Palanga</div>
+            <div class="role_equipe">Conseillère communale </div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>loredana1955@scarlet.be</div>
         </div>
 
     </div>
 
-<!--END ROW 4-->
+    <!--END ROW 4-->
 </div>
 
 
@@ -276,23 +218,36 @@
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/16.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/13.jpg ") ?>" alt="">
 
         </div>
 
         <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
-            <div class="elue_equipe">Carine Liekendael</div>
-            <div class="role_equipe">Conseillère CPAS </div>
-            <div class="email_equipe">carine.liekendael@gmail.com</div>
+            <div class="elue_equipe">Mohamed El Abboudi</div>
+            <div class="role_equipe">Conseiller communal et de police </div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>mo-elabboudi@hotmail.com</div>
         </div>
 
     </div>
 
 
     <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img class=" portrait_equipe" src="<?= base_url ("assets/images/equipe/17.jpg") ?>" alt="">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/18.jpg ") ?>" alt="">
+
+        </div>
+
+        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
+            <div class="elue_equipe">Nicole Linders</div>
+            <div class="role_equipe"></div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>niclinders@gmail.com</div>
+        </div>
+
+    </div>
+
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/17.jpg ") ?>" alt="">
 
         </div>
 
@@ -304,6 +259,56 @@
 
     </div>
 
+    <!--END ROW 4-->
+</div>
 
-<!--END ROW 4-->
+
+<!--DEBUT ROW-->
+<div class="row row_equipe text-center">
+
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/16.jpg ") ?>" alt="">
+
+        </div>
+
+        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
+            <div class="elue_equipe">Carine Liekendael</div>
+            <div class="role_equipe">Conseillère CPAS </div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>carine.liekendael@gmail.com</div>
+        </div>
+
+    </div>
+
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/11.jpg ") ?>" alt="">
+
+        </div>
+
+        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
+            <div class="elue_equipe">Luc Léonard</div>
+            <div class="role_equipe">Conseiller communal - Chef du groupe MR au conseil communal </div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i> luccjleonard@gmail.com</div>
+        </div>
+
+    </div>
+
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+            <img class=" portrait_equipe" src="<?= base_url("assets/images/equipe/14.jpg ") ?>" alt="">
+
+        </div>
+
+        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 block_equipe">
+            <div class="elue_equipe">Pascal Gardinal</div>
+            <div class="role_equipe">Conseiller CPAS - Secrétaire et trésorier de la section MR de Molenbeek-Saint-Jean </div>
+            <div class="email_equipe"><i class="fa fa-paper-plane plane_equipe" aria-hidden="true"></i>pascal.gardinal@skynet.be</div>
+        </div>
+
+    </div>
+
+
+    <!--END ROW 4-->
 </div>
