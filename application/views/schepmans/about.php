43280275e1e5d17eb5d55256d38ea21e8964135c
jerem jerem <jerem jerem>
2017-06-09 14:54:37 +0200

    page about version mobile

--- application/views/schepmans/about.php
+++ application/views/schepmans/about.php
@@ -1,76 +1,72 @@
-<img id="bannière" src="<?php echo base_url("assets/images/canall.jpg ")?>"/>
+<img id="banniere" src="<?php echo base_url("assets/images/canall.jpg ")?>"/>
 
-<?php
-//die(var_dump($this->lang->language));
-?>
 <div id="box_about">
 
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 
         <div class="row">
 
-            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-
-
-
-
-                    
-                  
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 
                 <h1 id="size_h1" title="Présentaion de Françoise Schepmans" class="title_about"> Françoise Schepmans</h1>
-                <article>
-                    <p class="p_about"> Françoise A.M. Schepmans (née le 18 juin 1960 à Berchem-Sainte-Agathe) est une femme politique belge,
-                        fille de Jacques Schepmans, dernier directeur général de l'hebdomadaire Pourquoi Pas ?.</p>
-                    <p class="p_about">Elle occupe actuellement les fonctions de députée fédérale, Vice-présidente de la Chambre des représentants
-                        ainsi que bourgmestre de la commune de Molenbeek-Saint-Jean.Le 3 mars 2008, elle est élue pour la
-                        première fois administratrice cooptée au conseil d'administration de l'Université libre de Bruxelles.</p>
-                </article>
+
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <img id="portraitschepmans" src="<?php echo base_url("assets/images/schepmans.jpg ")?>"/>
+            </div>
+
+        </div>
+
+        <!--end col 12-->
+    </div>
 
 
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 barre">
+
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+    
+            <article>
+                <p class="p_about"> Françoise A.M. Schepmans (née le 18 juin 1960 à Berchem-Sainte-Agathe) est une femme politique belge, fille
+                    de Jacques Schepmans, dernier directeur général de l'hebdomadaire Pourquoi Pas ?.</p>
+                <p class="p_about">Elle occupe actuellement les fonctions de députée fédérale, Vice-présidente de la Chambre des représentants
+                    ainsi que bourgmestre de la commune de Molenbeek-Saint-Jean.Le 3 mars 2008, elle est élue pour la première
+                    fois administratrice cooptée au conseil d'administration de l'Université libre de Bruxelles.</p>
                 <p class="p_about">
                     <?php echo $this->lang->line('aboutp1');?>
                 </p>
                 <p class="p_about">
                     <?php echo $this->lang->line('aboutp2');?>
                 </p>
+            </article>
 
-            </div>
+     </div>
 
+</div>
 
-            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                <img id="portraitschepmans" src="<?php echo base_url("assets/images/schepmans.jpg")?>"/>
-
-            </div>
-        </div>
-        <div class="barre">
-
-        </div>
-        <!--end col 12-->
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-
-
-        
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 margin_bottom_about">
 
         <h2 class="title_about" id="font-size">
             <?php echo $this->lang->line('abouth2');?>
         </h2>
-        <p class="p_about">
+        <article>
+            <p class="p_about">
                 <?php echo $this->lang->line('aboutp3');?>.
-        </p>
-        <p class="p_about"> 
-            <?php echo $this->lang->line('aboutp4');?>.
-        </p>
-        <br>
-        <p class="p_about">
-            <?php echo $this->lang->line('aboutp5');?>.
-        </p>
-        <p class="p_about">
-            <?php echo $this->lang->line('aboutp6');?>
-        </p>
-        <p class="p_about">
-            <?php echo $this->lang->line('aboutp7');?> 
-        </p>
+            </p>
+            <p class="p_about">
+                <?php echo $this->lang->line('aboutp4');?>.
+            </p>
+            <br>
+            <p class="p_about">
+                <?php echo $this->lang->line('aboutp5');?>.
+            </p>
+            <p class="p_about">
+                <?php echo $this->lang->line('aboutp6');?>
+            </p>
+            <p class="p_about">
+                <?php echo $this->lang->line('aboutp7');?>
+            </p>
+        </article>
     </div>
-    <!--end container-->
+    <!--end box_about-->
 </div>
